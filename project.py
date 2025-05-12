b709b39bb39c87f54d5d4a82718a3224866cadf4
DLim226 <dannylim607@gmail.com>
2025-05-12 16:14:14 -0400

    Update project.py

--- project.py
+++ project.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import expon
 ILI= pd.read_csv("ilidata.csv")
+ILI = ILI.sort_values(by="epiweek") 
+ILI["weeks"] = range(len(ILI)) 
 state=st.selectbox(
   "Choose a State:",
    ("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", 
