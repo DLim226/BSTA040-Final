00a1eed4c570c99d08cac6aff9b64865f6ad5b45
DLim226 <dannylim607@gmail.com>
2025-05-12 15:34:10 -0400

    Update project.py

--- project.py
+++ project.py
@@ -9,5 +9,6 @@ state=st.selectbox(
    ("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", 
 "VT", "WA", "WI", "WV", "WY"),
   index=None,
-  placeholder="Select a State"
+  placeholder="Select a State",
+)
 st.write("You selected:", option)
