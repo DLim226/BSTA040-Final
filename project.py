2ac7704430881cba07c03754afab0b9c51418fc5
DLim226 <dannylim607@gmail.com>
2025-05-12 10:45:53 -0400

    Update project.py

--- project.py
+++ project.py
@@ -3,3 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import expon
+def load_data():
+    df = pd.read_csv("ilidata.csv")
+    df = df[df['epiweek'].between(201040, 202540)]
+    df = df.sort_values(by='epiweek')
+    df['weeks'] = range(len(df))
+    return df
