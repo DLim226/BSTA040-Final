918a3515b8c709ab24f08b6620347a8232203c06
DLim226 <dannylim607@gmail.com>
2025-05-12 15:26:23 -0400

    Update project.py

--- project.py
+++ project.py
@@ -4,14 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import expon
 ILI= pd.read_csv("ilidata.csv")
-ILI=ILI.sort_values(by="epiweek")
-ILI["weeks"]=range(len(ILI))
 state=st.selectbox(
   "Choose a State:",
   ILI['state'],
   index=None,
   placeholder="Select a State"
-)
-if state:
-  state_data = ILI[ILI['state'] == state]
-  st.line_chart(state_data[['weeks', 'ili']].set_index('weeks'))
