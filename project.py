3bb4ff7db394b61f423fac3d52e907b0752235c8
DLim226 <dannylim607@gmail.com>
2025-05-12 16:19:18 -0400

    Update project.py

--- project.py
+++ project.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import expon
 ILI= pd.read_csv("ilidata.csv")
-ILI = ILI.sort_values(by="epiweek") 
-ILI["weeks"] = range(len(ILI)) 
 state=st.selectbox(
   "Choose a State:",
    ("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", 
@@ -16,7 +14,6 @@ state=st.selectbox(
 st.write("You selected:", state)
 if state:
   state_data = ILI[ILI["state"]==state]
-  st.write("Filtered data:", state_data[["weeks", "ili"]])
   st.line_chart(
   state_data,
   x="weeks",
