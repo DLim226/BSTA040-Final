042bfdbe36f5e87101bd81f6372e1c6a65e18990
DLim226 <dannylim607@gmail.com>
2025-05-12 17:45:22 -0400

    Update project.py

--- project.py
+++ project.py
@@ -18,6 +18,8 @@ state=st.selectbox(
 st.write("You selected:", state)
 if state:
   state_data = ILI[ILI["state"]==state]
+  state_data = state_data.sort_values(by="epiweek")
+  state_data["weeks"] = range(len(state_data)) 
   st.line_chart(
   state_data,
   x="weeks",
